efb59d204749b746e52527969b4ea0683b9c0c66
Yoshify <xyoshify@gmail.com>
2023-07-24 18:06:19 +1000

    🎨 Added formatting on save to save_py_file

--- src/pyconnectwise/utils/fs.py
+++ src/pyconnectwise/utils/fs.py
@@ -1,6 +1,33 @@
+import black
+from black import NothingChanged
+import isort
 
-def save_py_file(filepath:str, content:str):
-    if '.py' not in filepath:
-        filepath += '.py'
-    with open(filepath, 'w') as f:
-        f.write(content)
+
+def save_py_file(filepath: str, content: str):
+    """
+    Save Python file
+
+    This method takes a file path and content as input, and saves the content as a Python file with the specified file path. If the file path does not have a '.py' extension, it is automatically appended.
+
+    Parameters:
+    - filepath (str): The file path where the Python file should be saved. If the file path does not have a '.py' extension, it is automatically appended.
+    - content (str): The content that should be saved in the Python file.
+
+    Returns:
+    None
+
+    Example usage:
+    save_py_file("example/file", "print('Hello, World!')")
+    save_py_file("example/file.py", "print('Hello, World!')")
+    """
+    if ".py" not in filepath:
+        filepath += ".py"
+    with open(filepath, "w") as f:
+        try:
+            formatted_content = black.format_file_contents(
+                content, fast=False, mode=black.FileMode(line_length=120)
+            )
+        except NothingChanged:
+            formatted_content = content
+        formatted_content = isort.code(formatted_content, line_length=120)
+        f.write(formatted_content)
