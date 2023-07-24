35606fe9df1eb354f0eff4032d3235e38a68bbf4
Yoshify <xyoshify@gmail.com>
2023-07-24 18:07:31 +1000

    :bulb: Added docstrings

--- src/pyconnectwise/utils/helpers.py
+++ src/pyconnectwise/utils/helpers.py
@@ -3,11 +3,53 @@ import re
 from typing import Any
 
 
-def cw_format_datetime(datetime: datetime) -> str:
-    return datetime.strftime("%Y-%m-%dT%H:%M:%SZ")
+def cw_format_datetime(dt: datetime) -> str:
+    """Format a datetime object as a string in ISO 8601 format. This is the format that ConnectWise uses.
+
+    Args:
+        dt (datetime): The datetime object to be formatted.
+
+    Returns:
+        str: The formatted datetime string in the format "YYYY-MM-DDTHH:MM:SSZ".
+
+    Example:
+        from datetime import datetime
+
+        dt = datetime(2022, 1, 1, 12, 0, 0)
+        formatted_dt = cw_format_datetime(dt)
+        print(formatted_dt)  # Output: "2022-01-01T12:00:00Z"
+    """
+    return dt.strftime("%Y-%m-%dT%H:%M:%SZ")
 
 
-def parse_link_headers(headers: dict[str, str]) -> dict[str, Any]:
+def parse_link_headers(headers: dict[str, str]) -> dict[str, Any] | None:
+    """
+    Parses link headers to extract pagination information.
+
+    Arguments:
+    - headers: A dictionary containing the headers of an HTTP response. The value associated with the "Link" key should be a string representing the link headers.
+
+    Returns:
+    - A dictionary containing the extracted pagination information. The keys in the dictionary include:
+      - "first_page": An optional integer representing the number of the first page.
+      - "prev_page": An optional integer representing the number of the previous page.
+      - "next_page": An optional integer representing the number of the next page.
+      - "last_page": An optional integer representing the number of the last page.
+      - "has_next_page": A boolean indicating whether there is a next page.
+      - "has_prev_page": A boolean indicating whether there is a previous page.
+
+    If the "Link" header is not present in the headers dictionary, None is returned.
+
+    Example Usage:
+        headers = {
+            "Link": '<https://example.com/api?page=1>; rel="first", <https://example.com/api?page=2>; rel="next"'
+        }
+        pagination_info = parse_link_headers(headers)
+        print(pagination_info)
+        # Output: {'first_page': 1, 'next_page': 2, 'has_next_page': True}
+    """
+    if headers.get("Link") is None:
+        return None
     links = headers["Link"].split(",")
     has_next_page: bool = False
     has_prev_page: bool = False
