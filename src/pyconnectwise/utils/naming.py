619271fb1934fbd131a7e75c79bbdb04a2cb2c72
Yoshify <xyoshify@gmail.com>
2023-07-24 18:08:22 +1000

    :bug: Fix for fields being generated with reserved keywords Endpoints were generating reserved keywords as fields. If this occurs, it'll now append them with _

--- src/pyconnectwise/utils/naming.py
+++ src/pyconnectwise/utils/naming.py
@@ -1,9 +1,22 @@
+from keyword import iskeyword
+
+
 def to_snake_case(string: str) -> str:
-     return ('_' if string.startswith('_') else '') + ''.join(['_' + i.lower() if i.isupper() else i for i in string.lstrip('_')]).lstrip('_')
+    return ('_' if string.startswith('_') else '') + ''.join(
+        ['_' + i.lower() if i.isupper() else i for i in string.lstrip('_')]).lstrip('_')
+
 
 def to_camel_case(string: str) -> str:
     string_split = string.split("_")
     return string_split[0] + "".join(word.capitalize() for word in string_split[1:])
 
+
 def to_title_case_preserve_case(string: str) -> str:
-     return string[:1].upper() + string[1:]
+    return string[:1].upper() + string[1:]
+
+
+def ensure_not_reserved(string: str) -> str:
+    if iskeyword(string):
+        return string + '_'
+    else:
+        return string
