4cd8efb0f7c3c4e06b592851ec1ad1602e8cbd1e
Yoshify <xyoshify@gmail.com>
2023-08-22 15:11:54 +1000

    :fire: removed unnecessary code

--- src/pyconnectwise/models/base/connectwise_model.py
+++ src/pyconnectwise/models/base/connectwise_model.py
@@ -1,11 +1,6 @@
 from __future__ import annotations
 from pydantic import BaseModel, ConfigDict
 from pyconnectwise.utils.naming import to_camel_case
-from typing import Type, Any, TypeVar, get_type_hints
-from typing import ForwardRef
-
-Model = TypeVar("Model", bound="BaseModel")
-
 
 class ConnectWiseModel(BaseModel):
     model_config = ConfigDict(
