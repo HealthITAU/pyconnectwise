2309a2f9153c2b9d430eb7d2600689fae2eacc61
Scott Phillips <polygonguru@gmail.com>
2024-08-20 10:08:14 -0400

    fix: Allow downloading attachments

--- src/pyconnectwise/endpoints/manage/SystemDocumentsIdDownloadEndpoint.py
+++ src/pyconnectwise/endpoints/manage/SystemDocumentsIdDownloadEndpoint.py
@@ -1,3 +1,4 @@
+from requests import Response
 from typing import TYPE_CHECKING
 
 from pyconnectwise.endpoints.base.connectwise_endpoint import ConnectWiseEndpoint
@@ -9,3 +10,9 @@ if TYPE_CHECKING:
 class SystemDocumentsIdDownloadEndpoint(ConnectWiseEndpoint):
     def __init__(self, client: "ConnectWiseClient", parent_endpoint: ConnectWiseEndpoint = None) -> None:
         ConnectWiseEndpoint.__init__(self, client, "download", parent_endpoint=parent_endpoint)
+
+    def stream(self) -> Response:
+        """
+        Performs a GET request against the /system/documents/{id}/download endpoint and returns the response object, supporting streaming
+        """
+        return super()._make_request("GET", stream=True)
